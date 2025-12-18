09b624243ba598a28e72d235761fb14ebcb0fd27
Ogulcan Aydogan <contact@ogulcanaydogan.com>
2025-12-18 19:30:18 +0300

    Improve module docs and examples

--- main.tf
+++ main.tf
@@ -28,20 +28,16 @@ data "aws_ami" "amazon-linux-2" {
   }
 }
 
-data "template_file" "userdata" {
-  template = file("${abspath(path.module)}/userdata.sh")
-  vars = {
-    server-name = var.server-name
-  }
-}
-
 resource "aws_instance" "tfmyec2" {
-  ami = data.aws_ami.amazon-linux-2.id
-  instance_type = var.instance_type
-  count = var.num_of_instance
-  key_name = var.key_name
+  ami                    = data.aws_ami.amazon-linux-2.id
+  instance_type          = var.instance_type
+  count                  = var.num_of_instance
+  key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.tf-sec-gr.id]
-  user_data = data.template_file.userdata.rendered
+  user_data = templatefile("${abspath(path.module)}/userdata.sh", {
+    server-name = var["server-name"]
+  })
+
   tags = {
     Name = var.tag
   }
@@ -54,20 +50,20 @@ resource "aws_security_group" "tf-sec-gr" {
   }
 
   dynamic "ingress" {
-    for_each = var.docker-instance-ports
+    for_each = var["docker-instance-ports"]
     iterator = port
     content {
-      from_port = port.value
-      to_port = port.value
-      protocol = "tcp"
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
     }
   }
 
   egress {
-    from_port =0
-    protocol = "-1"
-    to_port =0
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,30 +1,35 @@
 variable "instance_type" {
   type = string
+  description = "EC2 instance type to launch for the Docker host."
   default = "t2.micro"
 }
 
 variable "key_name" {
   type = string
+  description = "Existing EC2 key pair name used to allow SSH access."
   default = "KeyPair"
 }
 
 variable "num_of_instance" {
   type = number
+  description = "Number of EC2 instances to create."
   default = 1
 }
 
 variable "tag" {
   type = string
+  description = "Name tag applied to created resources."
   default = "Docker-Instance"
 }
 
 variable "server-name" {
   type = string
+  description = "Hostname assigned to the instance via user data."
   default = "docker-instance"
 }
 
 variable "docker-instance-ports" {
   type = list(number)
-  description = "docker-instance-sec-gr-inbound-rules"
+  description = "List of TCP ports to allow for inbound traffic to the instance security group."
   default = [22, 80, 8080]
 }
